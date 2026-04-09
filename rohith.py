ca3f5c5a7c2c4409620a79408f181b2a246a2ae6
MonsterEcho <rohithkumarkolli54@gamil.com>
2026-04-09 20:46:36 +0530

    Initial

---
+++ rohith.py
@@ -0,0 +1,9 @@
+a = 10
+b = "Python"
+c = 10.5
+d = 2.14j
+
+print("Data type of Variable a :", type(a))
+print("Data type of Variable b :", type(b))
+print("Data type of Variable c :", type(c))
+print("Data type of Variable d :", type(d))
